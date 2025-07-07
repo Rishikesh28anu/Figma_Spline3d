bba396a3245bc9423a8e7644c53c42cfd2f94358
unknown <Rishikeshanu06@vitstudent.ac.in>
2025-07-07 19:05:23 +0530

    Add SplineViewer component

---
+++ src/View.js
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from "react";
+import { Application } from "@splinetool/runtime";
+
+export default function SplineViewer() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const app = new Application(canvas);
+    app.load("https://prod.spline.design/k57DRqusTJSxVZLX/scene.splinecode");
+  }, []);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{ width: "100vw", height: "100vh", display: "block" }}
+    ></canvas>
+  );
+}
